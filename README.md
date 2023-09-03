356449abcda057adbda1d2a500cda8fe42a8e27b
dineshpinto <annual.fallout_0z@icloud.com>
2023-09-03 10:46:10 +0200

    Added Python bindings

--- README.md
+++ README.md
@@ -2,11 +2,25 @@
 
 Rust implementation of a synchronous gossip protocol.
 
-## Build
+## Build Rust library
 
 ```bash
 git clone https://github.com/dineshpinto/gossip-protocol-rs.git
 cd gossip-protocol-rs
 cargo build --release
 cargo run --release
+```
+
+## Build Python bindings
+
+```bash
+cd gossip-protocol-rs
+poetry install
+poetry run maturin develop
+```
+
+```ipython
+>>> from gossip_protocol_rs import run_gossip_protocol
+>>> res = run_gossip_protocol(num_honest_sample=5, num_adversarial_sample=4,
+    num_non_sample=1000, num_peers=6, cycles=200)
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use state_transition_function::evolve_state;
 mod node;
 mod state_transition_function;
 
+#[pyfunction]
 pub fn run_gossip_protocol(
     num_honest_sample: usize,
     num_adversarial_sample: usize,
@@ -28,31 +29,8 @@ pub fn run_gossip_protocol(
     broadcasts
 }
 
-#[pyfunction]
-fn pyrun_gossip_protocol(
-    num_honest_sample: usize,
-    num_adversarial_sample: usize,
-    num_non_sample: usize,
-    num_peers: usize,
-    cycles: usize,
-) -> PyResult<Vec<f32>> {
-    // Create nodes
-    let mut nodes = create_nodes(
-        num_honest_sample,
-        num_adversarial_sample,
-        num_non_sample,
-    );
-
-    // Connect nodes together
-    connect_nodes_to_random_peers(&mut nodes, num_peers);
-
-    // Evolve state of the network
-    let broadcasts = evolve_state(&mut nodes, cycles);
-    Ok(broadcasts)
-}
-
 #[pymodule]
 fn gossip_protocol_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(pyrun_gossip_protocol, m)?)?;
+    m.add_function(wrap_pyfunction!(run_gossip_protocol, m)?)?;
     Ok(())
 }
