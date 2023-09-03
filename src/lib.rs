3ad3ef04855b2eb34477d3cbd5619292f326a2ed
dineshpinto <annual.fallout_0z@icloud.com>
2023-09-03 10:27:39 +0200

    Added Python bindings for PyO3

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use pyo3::prelude::*;
+
 use node::{connect_nodes_to_random_peers, create_nodes};
 use state_transition_function::evolve_state;
 
@@ -24,4 +26,33 @@ pub fn run_gossip_protocol(
     // Evolve state of the network
     let broadcasts = evolve_state(&mut nodes, cycles);
     broadcasts
+}
+
+#[pyfunction]
+fn pyrun_gossip_protocol(
+    num_honest_sample: usize,
+    num_adversarial_sample: usize,
+    num_non_sample: usize,
+    num_peers: usize,
+    cycles: usize,
+) -> PyResult<Vec<f32>> {
+    // Create nodes
+    let mut nodes = create_nodes(
+        num_honest_sample,
+        num_adversarial_sample,
+        num_non_sample,
+    );
+
+    // Connect nodes together
+    connect_nodes_to_random_peers(&mut nodes, num_peers);
+
+    // Evolve state of the network
+    let broadcasts = evolve_state(&mut nodes, cycles);
+    Ok(broadcasts)
+}
+
+#[pymodule]
+fn gossip_protocol_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(pyrun_gossip_protocol, m)?)?;
+    Ok(())
 }
